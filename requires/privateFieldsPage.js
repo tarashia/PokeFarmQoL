239e08af536981ebf7fdc8d2e5001c6dea888dd8
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2020-03-17 22:48:07 -0400

    Fix 'this' errors

--- requires/privateFieldsPage.js
+++ requires/privateFieldsPage.js
@@ -27,9 +27,10 @@ class PrivateFieldsPage {
 	this.typeArray = [];
 	this.natureArray = [];
 	this.eggGroupArray = [];
+	const obj = this
 	this.observer = new MutationObserver((mutations) => {
             mutations.forEach((mutation) => {
-		this.customSearch();
+		obj.customSearch();
             });
 	});
     }
@@ -136,65 +137,67 @@ class PrivateFieldsPage {
         });
     }
     setupHandlers() {
+	const obj = this
         $(window).on('load', (() => {
-            this.customSearch();
+            obj.customSearch();
         }));
 
-        $(document).on('load', '.field', (function(e) {
-            this.customSearch();
+        $(document).on('load', '.field', (function() {
+            obj.customSearch();
         }));
 
-        $(document).on('click', '*[data-menu="release"]', (function(e) { //select all feature
-            this.releaseEnableReleaseAll();
+        $(document).on('click', '*[data-menu="release"]', (function() { //select all feature
+            obj.releaseEnableReleaseAll();
         }));
 
-        $(document).on('click', '#addPrivateFieldTypeSearch', (function(e) { //add field type list
-            this.addSelectSearch('typeNumber', 'types', 'fieldType', GLOBALS.TYPE_OPTIONS, 'removePrivateFieldTypeSearch', 'fieldTypes');
-            this.customSearch();
+        $(document).on('click', '#addPrivateFieldTypeSearch', (function() { //add field type list
+            obj.addSelectSearch('typeNumber', 'types', 'fieldType', GLOBALS.TYPE_OPTIONS, 'removePrivateFieldTypeSearch', 'fieldTypes');
+            obj.customSearch();
         }));
 
-        $(document).on('click', '#removePrivateFieldTypeSearch', (function(e) { //remove field type list
-            typeArray = this.removeSelectSearch(typeArray, e, $(e).parent().find('select').val(), 'fieldType', 'fieldTypes')
-            this.saveSettings();
-            this.customSearch();
+        $(document).on('click', '#removePrivateFieldTypeSearch', (function() { //remove field type list
+            typeArray = obj.removeSelectSearch(typeArray, this, $(this).parent().find('select').val(), 'fieldType', 'fieldTypes')
+            obj.saveSettings();
+            obj.customSearch();
         }));
 
-        $(document).on('click', '#addPrivateFieldNatureSearch', (function(e) { //add field nature search
-            this.addSelectSearch('natureNumber', 'natures', 'fieldNature', GLOBALS.NATURE_OPTIONS, 'removePrivateFieldNature', 'natureTypes')
-            this.customSearch();
+        $(document).on('click', '#addPrivateFieldNatureSearch', (function() { //add field nature search
+            obj.addSelectSearch('natureNumber', 'natures', 'fieldNature', GLOBALS.NATURE_OPTIONS, 'removePrivateFieldNature', 'natureTypes')
+            obj.customSearch();
         }));
 
-        $(document).on('click', '#removePrivateFieldNature', (function(e) { //remove field nature search
-            natureArray = this.removeSelectSearch(typeArray, e, $(e).parent().find('select').val(), 'fieldNature', 'natureTypes')
-            this.saveSettings();
-            this.customSearch();
+        $(document).on('click', '#removePrivateFieldNature', (function() { //remove field nature search
+            natureArray = obj.removeSelectSearch(typeArray, this, $(this).parent().find('select').val(), 'fieldNature', 'natureTypes')
+            obj.saveSettings();
+            obj.customSearch();
         }));
 
-        $(document).on('click', '#addPrivateFieldEggGroupSearch', (function(e) { //add egg group nature search
-            this.addSelectSearch('eggGroupNumber', 'eggGroups', 'fieldEggGroup', GLOBALS.EGG_GROUP_OPTIONS, 'removePrivateFieldEggGroupSearch', 'eggGroupTypes')
-            this.customSearch();
+        $(document).on('click', '#addPrivateFieldEggGroupSearch', (function() { //add egg group nature search
+            obj.addSelectSearch('eggGroupNumber', 'eggGroups', 'fieldEggGroup', GLOBALS.EGG_GROUP_OPTIONS, 'removePrivateFieldEggGroupSearch', 'eggGroupTypes')
+            obj.customSearch();
         }));
 
-        $(document).on('click', '#removePrivateFieldEggGroup', (function(e) { //remove egg group nature search
-            eggGroupArray = this.removeSelectSearch(typeArray, e, $(e).parent().find('select').val(), 'fieldEggGroup', 'eggGroupTypes')
-            this.saveSettings();
-            this.customSearch();
+        $(document).on('click', '#removePrivateFieldEggGroup', (function() { //remove egg group nature search
+            eggGroupArray = obj.removeSelectSearch(typeArray, this, $(this).parent().find('select').val(), 'fieldEggGroup', 'eggGroupTypes')
+            obj.saveSettings();
+            obj.customSearch();
         }));
 
-        $(document).on('change', '.qolsetting', (function(e) {
-            this.loadSettings();
-            this.customSearch();
-            this.saveSettings();
+        $(document).on('change', '.qolsetting', (function() {
+            obj.loadSettings();
+            obj.customSearch();
+            obj.saveSettings();
         }));
 
-        $(document).on('input', '.qolsetting', (function(e) { //Changes QoL settings
-            this.settingsChange(this.getAttribute('data-key'), $(e).val(), $(e).parent().parent().attr('class'), $(this).parent().attr('class'));
-            this.customSearch();
-            this.saveSettings();
+        $(document).on('input', '.qolsetting', (function() { //Changes QoL settings
+            obj.settingsChange(this.getAttribute('data-key'), $(this).val(),
+			       $(this).parent().parent().attr('class'), $(this).parent().attr('class'));
+            obj.customSearch();
+            obj.saveSettings();
         }));
 
-        $(document).on('click', '*[data-menu="bulkmove"]', (function(e) { // select all feature
-            this.moveEnableReleaseAll();
+        $(document).on('click', '*[data-menu="bulkmove"]', (function() { // select all feature
+            obj.moveEnableReleaseAll();
         }));
 
     }
@@ -212,17 +215,17 @@ class PrivateFieldsPage {
 
         //loop to find all the types
         if (filteredTypeArray.length > 0 || filteredNatureArray.length > 0 || filteredEggGroupArray.length > 0) {
-            $('.fieldmon').each(function(e) {
-                let searchPokemonBigImg = $(e)[0].childNodes[0];
+            $('.fieldmon').each(function() {
+                let searchPokemonBigImg = $(this)[0].childNodes[0];
                 let searchPokemon = searchPokemonBigImg.alt;
                 let searchPokemonIndex = dexData.indexOf('"'+searchPokemon+'"');
                 let searchTypeOne = dexData[searchPokemonIndex + 1];
                 let searchTypeTwo = dexData[searchPokemonIndex + 2];
 
-                let searchNature = $($(e).next()[0].querySelector('.fieldmontip')).children(':contains(Nature)')[0].innerText.split(" ")[1];
+                let searchNature = $($(this).next()[0].querySelector('.fieldmontip')).children(':contains(Nature)')[0].innerText.split(" ")[1];
                 if (searchNature.indexOf("(") > -1) { searchNature = searchNature.slice(0, -1); }
 
-                let searchEggGroup = $($(e).next()[0].querySelector('.fieldmontip')).
+                let searchEggGroup = $($(this).next()[0].querySelector('.fieldmontip')).
                     children(':contains(Egg Group)')[0].innerText.slice("Egg Group: ".length)
 
                 for (let i = 0; i < filteredTypeArray.length; i++) {
@@ -270,76 +273,76 @@ class PrivateFieldsPage {
     releaseEnableReleaseAll() {
         if(this.settings.releaseSelectAll === true) {
             document.querySelector('.dialog>div>div>div>div>button').insertAdjacentHTML('afterend', '<label id="selectallfield"><input id="selectallfieldcheckbox" type="checkbox">Select all  </label><label id="selectallfieldany"><input id="selectallfieldanycheckbox" type="checkbox">Select Any  </label><label id="selectallfieldsour"><input id="selectallfieldsourcheckbox" type="checkbox">Select Sour  </label><label id="selectallfieldspicy"><input id="selectallfieldspicycheckbox" type="checkbox">Select Spicy</label><label id="selectallfielddry"><input id="selectallfielddrycheckbox" type="checkbox">Select Dry  </label><label id="selectallfieldsweet"><input id="selectallfieldsweetcheckbox" type="checkbox">Select Sweet  </label><label id="selectallfieldbitter"><input id="selectallfieldbittercheckbox" type="checkbox">Select Bitter  </label>');
-            $('#selectallfieldcheckbox').click(function(e) {
-                $('#massreleaselist>ul>li>label>input').not(e).prop('checked', this.checked);
+            $('#selectallfieldcheckbox').click(function() {
+                $('#massreleaselist>ul>li>label>input').not(this).prop('checked', this.checked);
             });
 
-            $('#selectallfieldanycheckbox').click(function(e) {
+            $('#selectallfieldanycheckbox').click(function() {
                 let selectAny = $('.icons:contains("Any")').prev().prev().prev('input');
-                $(selectAny).not(e).prop('checked', e.checked);
+                $(selectAny).not(this).prop('checked', this.checked);
             });
 
-            $('#selectallfieldsourcheckbox').click(function(e) {
+            $('#selectallfieldsourcheckbox').click(function() {
                 let selectSour = $('.icons:contains("Sour")').prev().prev().prev('input');
-                $(selectSour).not(e).prop('checked', e.checked);
+                $(selectSour).not(this).prop('checked', this.checked);
             });
 
-            $('#selectallfieldspicycheckbox').click(function(e) {
+            $('#selectallfieldspicycheckbox').click(function() {
                 let selectSpicy = $('.icons:contains("Spicy")').prev().prev().prev('input');
-                $(selectSpicy).not(e).prop('checked', e.checked);
+                $(selectSpicy).not(this).prop('checked', this.checked);
             });
 
-            $('#selectallfielddrycheckbox').click(function(e) {
+            $('#selectallfielddrycheckbox').click(function() {
                 let selectDry = $('.icons:contains("Dry")').prev().prev().prev('input');
-                $(selectDry).not(e).prop('checked', e.checked);
+                $(selectDry).not(this).prop('checked', this.checked);
             });
 
-            $('#selectallfieldsweetcheckbox').click(function(e) {
+            $('#selectallfieldsweetcheckbox').click(function() {
                 let selectSweet = $('.icons:contains("Sweet")').prev().prev().prev('input');
-                $(selectSweet).not(e).prop('checked', e.checked);
+                $(selectSweet).not(this).prop('checked', this.checked);
             });
 
-            $('#selectallfieldbittercheckbox').click(function(e) {
+            $('#selectallfieldbittercheckbox').click(function() {
                 let selectBitter = $('.icons:contains("Bitter")').prev().prev().prev('input');
-                $(selectBitter).not(e).prop('checked', e.checked);
+                $(selectBitter).not(this).prop('checked', this.checked);
             });
         } // if
     } // releaseAll
     moveEnableReleaseAll() {
         if(this.settings.releaseSelectAll === true) {
             document.querySelector('.dialog>div>div>div>div>button').insertAdjacentHTML('afterend', '<label id="movefieldselectall"><input id="movefieldselectallcheckbox" type="checkbox">Select all  </label><label id="movefieldselectany"><input id="movefieldselectanycheckbox" type="checkbox">Select Any  </label><label id="movefieldselectsour"><input id="movefieldselectsourcheckbox" type="checkbox">Select Sour  </label><label id="movefieldselectspicy"><input id="movefieldselectspicycheckbox" type="checkbox">Select Spicy</label><label id="movefieldselectdry"><input id="movefieldselectdrycheckbox" type="checkbox">Select Dry  </label><label id="movefieldselectsweet"><input id="movefieldselectsweetcheckbox" type="checkbox">Select Sweet  </label><label id="movefieldselectbitter"><input id="movefieldselectbittercheckbox" type="checkbox">Select Bitter  </label>');
-            $('#movefieldselectallcheckbox').click(function(e) {
-                $('#massmovelist>ul>li>label>input').not(e).prop('checked', e.checked);
+            $('#movefieldselectallcheckbox').click(function() {
+                $('#massmovelist>ul>li>label>input').not(this).prop('checked', this.checked);
             });
 
-            $('#movefieldselectanycheckbox').click(function(e) {
+            $('#movefieldselectanycheckbox').click(function() {
                 let selectAny = $('.icons:contains("Any")').prev().prev().prev('input');
-                $(selectAny).not(e).prop('checked', e.checked);
+                $(selectAny).not(this).prop('checked', this.checked);
             });
 
-            $('#movefieldselectsourcheckbox').click(function(e) {
+            $('#movefieldselectsourcheckbox').click(function() {
                 let selectSour = $('.icons:contains("Sour")').prev().prev().prev('input');
-                $(selectSour).not(e).prop('checked', e.checked);
+                $(selectSour).not(this).prop('checked', this.checked);
             });
 
-            $('#movefieldselectspicycheckbox').click(function(e) {
+            $('#movefieldselectspicycheckbox').click(function() {
                 let selectSpicy = $('.icons:contains("Spicy")').prev().prev().prev('input');
-                $(selectSpicy).not(e).prop('checked', e.checked);
+                $(selectSpicy).not(this).prop('checked', this.checked);
             });
 
-            $('#movefieldselectdrycheckbox').click(function(e) {
+            $('#movefieldselectdrycheckbox').click(function() {
                 let selectDry = $('.icons:contains("Dry")').prev().prev().prev('input');
-                $(selectDry).not(e).prop('checked', e.checked);
+                $(selectDry).not(this).prop('checked', this.checked);
             });
 
-            $('#movefieldselectsweetcheckbox').click(function(e) {
+            $('#movefieldselectsweetcheckbox').click(function() {
                 let selectSweet = $('.icons:contains("Sweet")').prev().prev().prev('input');
-                $(selectSweet).not(e).prop('checked', e.checked);
+                $(selectSweet).not(this).prop('checked', this.checked);
             });
 
-            $('#movefieldselectbittercheckbox').click(function(e) {
+            $('#movefieldselectbittercheckbox').click(function() {
                 let selectBitter = $('.icons:contains("Bitter")').prev().prev().prev('input');
-                $(selectBitter).not(e).prop('checked', e.checked);
+                $(selectBitter).not(this).prop('checked', this.checked);
             });
         } // if
     } // moveEnableReleaseAll
