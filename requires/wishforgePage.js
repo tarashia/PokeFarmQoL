aa1e3904484adce0c34cbb070911a2891ede0412
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2020-08-02 07:27:20 -0400

    Working draft of condensed table

--- requires/wishforgePage.js
+++ requires/wishforgePage.js
@@ -12,18 +12,18 @@ class WishforgePage extends Page {
         });
         */
     } // constructor
-    
+
     setupHTML() {
         // setup table format
         const header = "<th>Type</th> <th>Level</th> <th>Gem Progress</th> <th>Item</th> <th>Upgrade</th> <th>Notify</th>";
-        
+
         // use GLOBALS.TYPE_LIST to get list of types
         const types = GLOBALS.TYPE_LIST;
-        
+
         // build HTML table
         let rows = {};
         for (let i = 0; i < types.length; i++) {
-            rows[types[i]] = `<td>${types[i]}</td> <td></td> <td>Test</td> <td>Test</td> <td>Test</td> <td>Test</td>`
+            rows[types[i]] = `<td>${types[i]}</td> <td></td> <td></td> <td></td> <td></td> <td></td>`
         };
         let table = `<table style="width: 100%">` +
             `<colgroup>` +
@@ -54,27 +54,52 @@ class WishforgePage extends Page {
         // move elements from original elements to table
         for (let i = 0; i < types.length; i++) {
             let index = i + 1;
+            let li = $(craftedBadgesList.children()[index])
 
             // get badge image
-            let badgeImg = $($($(craftedBadgesList.children()[index]).children()[0]).children()[0]);
+            let badgeImg = $($(li.children()[0]).children()[0]);
             badgeImg.appendTo(`tr#${types[i]}>td:nth-child(${LEVEL_COL})`);
 
             // get badge name
-            let badgeName = $($(craftedBadgesList.children()[index]).children()[0]); // .innerText;
+            let badgeName = $(li.children()[0]);
             badgeName.text(' ' + badgeName.text().replace(` ${types[i]} Badge`, ""));
             badgeName.css('display', 'inline-block');
             badgeName.appendTo(`tr#${types[i]}>td:nth-child(${LEVEL_COL})`);
 
             // get gem progress bar
+            let gemProgress = $(li.children()[0]);
+            gemProgress.appendTo(`tr#${types[i]}>td:nth-child(${GEM_COL})`);
+
+            // if the badge is under construction, the tooltip will not be there
+            if($(li.children()[0]).hasClass('itemtooltip')) {
+                let gemTooltip = $(li.children()[0]);
+                gemTooltip.appendTo(`tr#${types[i]}>td:nth-child(${GEM_COL})`);
+            }
 
             // get item progress bar
+            let itemProgress = $(li.children()[0]);
+            itemProgress.appendTo(`tr#${types[i]}>td:nth-child(${ITEM_COL})`);
 
-            // get upgrade button
+            // if the badge is under construction, the tooltip will not be there
+            if($(li.children()[0]).hasClass('itemtooltip')) {
+                let itemTooltip = $(li.children()[0]);
+                itemTooltip.appendTo(`tr#${types[i]}>td:nth-child(${ITEM_COL})`);
+            }
 
             // get notify button
+            let notifyBtn = $(li.children()[0]);
+            notifyBtn.appendTo(`tr#${types[i]}>td:nth-child(${NOTIFY_COL})`);
 
+            // get upgrade button
+            let updateBtn = $(li.children()[0]);
+            updateBtn.appendTo(`tr#${types[i]}>td:nth-child(${UPDATE_COL})`);
         }
 
+        // remove the li's left over
+        const children = craftedBadgesList.children();
+        for (let i = types.length; i >= 1; i--) {
+            $(children[i]).remove();
+        }
     }
 };
 
