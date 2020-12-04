bde5ce19fc06baf23f489d4282a466087d374caf
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2020-12-03 23:27:05 -0500

    Add test for QoL Hub controls

--- __tests__/compiled_test.js
+++ __tests__/compiled_test.js
@@ -1,207 +1,44 @@
-it("Stop failure", () => { expect(true).toBe(true) });
-// const jQuery = require('../__mocks__/jquery_files').jQuery;
-// const console = require('../__mocks__/console_suppress').console;
-// const fs = require('fs')
-// const path = require('path')
+const jQuery = require('../__mocks__/jquery_files').jQuery;
+const console = require('../__mocks__/console_suppress').console;
+const fs = require('fs')
+const path = require('path')
 
-// const pfqol = require('./compiled');
+const pfqol = require('./compiled');
 
-// const oldWindowLocation = window.location;
+const oldWindowLocation = window.location;
 
-// beforeAll(() => {
-//     delete window.location;
+beforeAll(() => {
+    delete window.location;
 
-//     window.location = Object.defineProperties(
-//         {},
-//         {
-//             ...Object.getOwnPropertyDescriptors(oldWindowLocation),
-//             href: {
-//                 writable: true,
-//                 value: 'fdsa'
-//             },
-//             assign: {
-//                 configurable: true,
-//                 value: jest.fn(),
-//             },
-//         },
-//     );
-// });
+    window.location = Object.defineProperties(
+        {},
+        {
+            ...Object.getOwnPropertyDescriptors(oldWindowLocation),
+            href: {
+                writable: true,
+                value: 'fdsa'
+            },
+            assign: {
+                configurable: true,
+                value: jest.fn(),
+            },
+        },
+    );
+});
 
-// describe("Test that PFQoL compiles", () => {
-//     test("Test QoL Hub controls", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'party.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         document.documentElement.innerHTML = innerHTML;
-//         global.location.href = "https://pokefarm.com/party";
-//         pfqol.pfqol(jQuery);
-//         jQuery('li[data-name="QoL"]').eq(0).trigger('click');
-//         jQuery('#updateDex').eq(0).trigger('click');
-//         jQuery('#resetPageSettings').eq(0).trigger('click');
-//         jQuery('#clearCachedDex').eq(0).trigger('click');
-//         jQuery('h3.slidermenu').trigger('click');
-//         jQuery('.closeHub').eq(0).trigger('click');
-//     });
-//     test("Test that PFQoL compiles on Daycare page", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'daycare_with_dialog.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/daycare";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//         // remove fieldmon and tooltip_content to force mutation observer
-//         jQuery('#fs_pokemon .fieldmon').eq(0).remove();
-//         jQuery('#fs_pokemon .tooltip_content').eq(0).remove();
-//     });
-//     test("Test that PFQoL Dex page handles click to enable on second type search while first type is not selected", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'dex.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/dex";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//         // mimic click outside the types list
-//         const typesSpan = $('.filter-type-2 .types');
-//         const type2 = $('.filter-type-2');
-//         let event = jQuery.Event("mousedown.dextfilter");
-//         event.originalEvent = {
-//             preventDefault: () => { return true; }
-//         };
-//         typesSpan.offset({
-//             top: 0,
-//             left: 462.5
-//         });
-//         typesSpan.width(162);
-//         event.pageX = 160;
-//         type2.trigger(event);
-//     });
-
-//     test("Test that PFQoL Dex page handles click to disable second type search", () => {
-//         // mimic click inside the types list
-//         const typesSpan = $('.filter-type-2 .types');
-//         const type2 = $('.filter-type-2');
-//         let event = jQuery.Event("mousedown.dextfilter");
-//         event.originalEvent = {
-//             preventDefault: () => { return true; }
-//         };
-//         typesSpan.offset({
-//             top: 0,
-//             left: 462.5
-//         });
-//         typesSpan.width(162);
-//         event.pageX = 160;
-//         type2.trigger(event);
-//     });
-
-//     test("Test that PFQoL Dex page handles click to enable on second type search while first type is selected", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'dex.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/dex";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//         // select a type in the first type list
-//         const types1Span = $('.filter-type:not(.filter-type-2) .types');
-//         const normal = types1Span.children().eq(0);
-//         normal.addClass("selected");
-//         // mimic click inside the types list
-//         const types2Span = $('.filter-type-2 .types');
-//         const type2 = $('.filter-type-2');
-//         let event = jQuery.Event("mousedown.dextfilter");
-//         event.originalEvent = {
-//             preventDefault: () => { return true; }
-//         };
-//         types2Span.offset({
-//             top: 0,
-//             left: 462.5
-//         });
-//         types2Span.width(162);
-//         event.pageX = 160;
-//         type2.trigger(event);
-//     });
-        
-//     test("Test that PFQoL Dex page handles clicks outside second type search", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'dex.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/dex";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//         // mimic click outside the types list
-//         const typesSpan = $('.filter-type-2 .types');
-//         const type2 = $('.filter-type-2');
-//         let event = jQuery.Event("mousedown.dextfilter");
-//         event.originalEvent = {
-//             preventDefault: () => { return true; }
-//         };
-//         typesSpan.offset({
-//             top: 0,
-//             left: 462.5
-//         });
-//         typesSpan.width(162);
-//         event.pageX = 511;
-//         type2.trigger(event);
-
-//     });
-//     test("Test that PFQoL compiles on Farm page", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'farm.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/farm";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//     });
-//     test("Test that PFQoL compiles on Fishing page", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'fishing.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/fishing";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//     });
-//     test("Test that PFQoL compiles on Lab page", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'lab.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/lab";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//     });
-//     test("Test that PFQoL compiles on Multiuser page", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'multiuser.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/users/A,B,C,D,E,F,G,H,I,J";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//     });
-//     test("Test that PFQoL compiles on Public Fields page", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'publicFields.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/fields/A";
-//         document.documentElement.innerHTML = innerHTML;
-//         pfqol.pfqol(jQuery);
-//     });
-//     test("Test that PFQoL compiles on Wishforge page", () => {
-//         const htmlpath = path.join(__dirname, './data/', 'wishforge_no_table.html');
-//         const html = fs.readFileSync(htmlpath, 'utf8', 'r');
-//         const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
-//         global.location.href = "https://pokefarm.com/forge";
-//         document.documentElement.innerHTML = innerHTML;
-
-//         // // grab the crafted badges HTML before it's changed so we can use it to mimic a mutation
-//         // const craftedBadgesList = $('#badges').clone();
-//         // const craftedBadges = craftedBadgesList.children();
-
-//         pfqol.pfqol(jQuery);
-
-//         // // trigger mutation observer
-//         // // the real mutation that will trigger the observer will be when PFQ remakes the crafted
-//         // // badges list, so let's mimic that
-//         // $('#badges').children().remove();
-//         // craftedBadges.each((i, v) => {
-//         //     $('#badges').append(v);
-//         // })
-//     });
-// });
+describe("Test that PFQoL compiles", () => {
+    test("Test QoL Hub controls", () => {
+        const htmlpath = path.join(__dirname, './data/', 'party.html');
+        const html = fs.readFileSync(htmlpath, 'utf8', 'r');
+        const innerHTML = html.replace(/<html .*?>/, '').replace(/<\/html>/, '').trim();
+        document.documentElement.innerHTML = innerHTML;
+        global.location.href = "https://pokefarm.com/party";
+        pfqol.pfqol(jQuery);
+        jQuery('li[data-name="QoL"]').eq(0).trigger('click');
+        jQuery('#updateDex').eq(0).trigger('click');
+        jQuery('#resetPageSettings').eq(0).trigger('click');
+        jQuery('#clearCachedDex').eq(0).trigger('click');
+        jQuery('h3.slidermenu').trigger('click');
+        jQuery('.closeHub').eq(0).trigger('click');
+    });
+});
