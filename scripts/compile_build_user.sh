524c06b3ff3902af1b9a9f523cd8628bdf1008aa
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-01-19 23:05:27 -0500

    Move bash scripts

---
+++ scripts/compile_build_user.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Compiling code into one js file..."
+
+# Order from Poke-Farm-QoL.user.js
+ROOT="."
+
+declare -a INPUT=("${ROOT}/requires/user/header.txt"
+                  "${ROOT}/requires/common/resources.js"
+                  "${ROOT}/requires/common/helpers.js"
+                  "${ROOT}/requires/common/globals.js"
+                  "${ROOT}/requires/user/evolutionTreeParser.js"
+                  "${ROOT}/requires/user/dexPageParser.js"
+                  "${ROOT}/requires/common/localStorageManager.js"
+                  "${ROOT}/requires/user/dexUtilities.js"
+                  "${ROOT}/requires/common/qolHub.js"
+                  "${ROOT}/requires/common/basePage.js"
+                  "${ROOT}/requires/common/shelterPage.js"
+                  "${ROOT}/requires/common/privateFieldsPage.js"
+                  "${ROOT}/requires/common/publicFieldsPage.js"
+                  "${ROOT}/requires/common/labPage.js"
+                  "${ROOT}/requires/common/fishingPage.js"
+                  "${ROOT}/requires/common/multiuserPage.js"
+                  "${ROOT}/requires/common/farmPage.js"
+                  "${ROOT}/requires/common/daycarePage.js"
+                  "${ROOT}/requires/common/dexPage.js"
+                  "${ROOT}/requires/common/wishforgePage.js"
+                  "${ROOT}/requires/common/pfqol.js"
+                  )
+OUTPUT="${ROOT}/Poke-Farm-QoL.user.js"
+rm -f "${OUTPUT}"
+
+for FILE in "${INPUT[@]}"; do
+   cat "$FILE" >> "${OUTPUT}"
+   echo "" >> "${OUTPUT}"
+done
+
+./scripts/remove_eslint_comments.sh "${OUTPUT}" "/tmp/output.txt"
+
+echo "Compilation complete!: ${OUTPUT}"
---
+++ scripts/compile_test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+echo "Compiling code into one js file..."
+
+# Order from Poke-Farm-QoL.user.js
+ROOT="."
+declare -a EXTERNALS=("${ROOT}/node_modules/jquery/dist/jquery.min.js"
+                     )
+declare -a INPUT=("${ROOT}/requires/user/header.txt"
+                  "${ROOT}/requires/common/resources.js"
+                  "${ROOT}/requires/common/helpers.js"
+                  "${ROOT}/requires/common/globals.js"
+                  "${ROOT}/requires/user/evolutionTreeParser.js"
+                  "${ROOT}/requires/user/dexPageParser.js"
+                  "${ROOT}/requires/common/localStorageManager.js"
+                  "${ROOT}/requires/user/dexUtilities.js"
+                  "${ROOT}/requires/common/qolHub.js"
+                  "${ROOT}/requires/common/basePage.js"
+                  "${ROOT}/requires/common/shelterPage.js"
+                  "${ROOT}/requires/common/privateFieldsPage.js"
+                  "${ROOT}/requires/common/publicFieldsPage.js"
+                  "${ROOT}/requires/common/labPage.js"
+                  "${ROOT}/requires/common/fishingPage.js"
+                  "${ROOT}/requires/common/multiuserPage.js"
+                  "${ROOT}/requires/common/farmPage.js"
+                  "${ROOT}/requires/common/daycarePage.js"
+                  "${ROOT}/requires/common/dexPage.js"
+                  "${ROOT}/requires/common/wishforgePage.js"
+                  "${ROOT}/requires/common/pfqol.js"
+                  )
+OUTPUT="${ROOT}/__tests__/compiled.js"
+
+# try to mimic the web environment
+echo "" > "${OUTPUT}"
+echo "/* instanbul ignore next */" >> "${OUTPUT}"
+echo "// eslint-disable-next-line camelcase" >>  "${OUTPUT}"
+echo "const GM_addStyle        = require('../__mocks__/tampermonkey').GM_addStyle;" >> "${OUTPUT}"
+
+for FILE in "${EXTERNALS[@]}"; do
+   echo "/* istanbul ignore next */" >> "${OUTPUT}"
+   echo "/* eslint-disable */" >>  "${OUTPUT}"
+   cat "$FILE" >> "${OUTPUT}"
+   echo "/* eslint-enable */" >>  "${OUTPUT}"
+   echo "" >> "${OUTPUT}"
+done
+
+for FILE in "${INPUT[@]}"; do
+   cat "$FILE" >> "${OUTPUT}"
+   echo "" >> "${OUTPUT}"
+done
+
+echo "Compilation complete!: ${OUTPUT}"
