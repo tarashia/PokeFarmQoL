49742c2b7db026d41b6143a3dcd31185c8547795
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-03-17 20:03:34 -0400

    Create sanctioend dex page

---
+++ requires/sanctioned/dexPage.js
@@ -0,0 +1,16 @@
+/* globals DexPageBase */
+// eslint-disable-next-line no-unused-vars
+class DexPage extends DexPageBase {
+    constructor(jQuery) {
+        super(jQuery);
+
+        // when entering the dex page, update the local storage QoLPokedex
+        // so the user can update their information
+        if(localStorage.getItem('QoLPokedex') !== null) {
+            if(jQuery('script#dexdata')) {
+                const text = jQuery('script#dexdata').text();
+                localStorage.setItem('QoLPokedex', text);
+            }
+        }
+    }
+}
