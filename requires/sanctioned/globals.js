4fbd90b3b6ee5b179440dc6ad8907fb3bbf834b5
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-03-17 20:03:44 -0400

    Create sanctioned globals

--- requires/sanctioned/globals.js
+++ requires/sanctioned/globals.js
@@ -1,4 +1,13 @@
 /* globals GlobalsBase */
 // eslint-disable-next-line no-unused-vars
 class Globals extends GlobalsBase {
+    constructor() {
+        super();
+
+        // load the dex from local storage if it exists
+        const dex = localStorage.getItem('QoLPokedex');
+        if(dex !== null) {
+            this.DEX_DATA = JSON.parse(localStorage.getItem('QoLPokedex'));
+        }
+    }
 }
