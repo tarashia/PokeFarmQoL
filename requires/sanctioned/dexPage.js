0c58fd7bbf5323abe3e0b07b042b1e3d27d85de7
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-03-22 21:59:14 -0400

    Only update dex data if it is present

--- requires/sanctioned/dexPage.js
+++ requires/sanctioned/dexPage.js
@@ -7,7 +7,7 @@ class DexPage extends DexPageBase {
         // when entering the dex page, update the local storage QoLPokedex
         // so the user can update their information
         if(localStorage.getItem('QoLPokedex') !== null) {
-            if(jQuery('script#dexdata')) {
+            if(jQuery('script#dexdata') && jQuery('script#dexdata').text()) {
                 const text = jQuery('script#dexdata').text();
                 localStorage.setItem('QoLPokedex', text);
             }
