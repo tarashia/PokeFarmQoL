e5acf009397e1d701eb6a591d0aab73fa9bc229e
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-03-26 11:23:11 -0400

    Add globals parameter

--- requires/user/dexPage.js
+++ requires/user/dexPage.js
@@ -1,7 +1,7 @@
 /* globals DexPageBase */
 // eslint-disable-next-line no-unused-vars
 class DexPage extends DexPageBase {
-    constructor(jQuery, localStorageMgr) {
-        super(jQuery, localStorageMgr);
+    constructor(jQuery, localStorageMgr, GLOBALS) {
+        super(jQuery, localStorageMgr, GLOBALS);
     }
 }
