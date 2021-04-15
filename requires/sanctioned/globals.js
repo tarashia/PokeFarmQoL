60264819ae1fe03f3a7bbd7ff9f86d52ab989f27
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-04-15 06:32:02 -0400

    Add error handling on fetch

--- requires/sanctioned/globals.js
+++ requires/sanctioned/globals.js
@@ -6,20 +6,54 @@ class Globals extends GlobalsBase {
         this.jQuery = jQuery;
         this.localStorageMgr = localStorageMgr;
 
+        const errorSuppressorKey = 'QoLFetchErrorCount';
+        const maxFetchErrorDisplays = 10;
+        let errorSuppressorCount = this.localStorageMgr.getItem(errorSuppressorKey);
+        if (errorSuppressorCount === null) {
+            this.localStorageMgr.setItem(errorSuppressorKey, JSON.stringify(0));
+            errorSuppressorCount = 0;
+        } else {
+            errorSuppressorCount = JSON.parse(errorSuppressorCount);
+        }
+
         // load the dex from local storage if it exists
         if (!this.localStorageMgr.loadDexIntoGlobalsFromStorage(this)) {
             const obj = this;
             // fetch the initial dex data from the /dex page
             fetch('/dex')
-                .then(r => r.text())
+                .then(r => {
+                    if (!r.ok) {
+                        throw new Error(`Response was not ok. OK=${r.ok}, Status=${r.status}, Text=${r.statusText}`);
+                    }
+                    return r.text();
+                })
                 .then(html => {
-                    const parser = new DOMParser();
-                    const doc = parser.parseFromString(html, 'text/html');
-                    const script = doc.getElementById('dexdata');
-                    const json = JSON.parse(script.textContent);
-                    obj.DEX_DATA = json;
-                    obj.localStorageMgr.updateLocalStorageDex(obj.jQuery, document, undefined, obj);
-                    obj.localStorageMgr.loadDexIntoGlobalsFromStorage(obj);
+                    try {
+                        const parser = new DOMParser();
+                        const doc = parser.parseFromString(html, 'text/html');
+                        const script = doc.getElementById('dexdata');
+                        const json = JSON.parse(script.textContent);
+                        obj.DEX_DATA = json;
+                        obj.localStorageMgr.updateLocalStorageDex(obj.jQuery, document, undefined, obj);
+                        obj.localStorageMgr.loadDexIntoGlobalsFromStorage(obj);
+                        errorSuppressorCount = 0;
+                        this.localStorageMgr.setItem(errorSuppressorKey, JSON.stringify(0));
+                    } catch (error) {
+                        if (errorSuppressorCount < maxFetchErrorDisplays) {
+                            errorSuppressorCount++;
+                            this.localStorageMgr.setItem(errorSuppressorKey, JSON.stringify(errorSuppressorCount));
+                            window.alert(`The following error occurred after successful fetching. Please include this message when reporting this issue.\n${error}\n` +
+                                `This message will be reported ${maxFetchErrorDisplays - errorSuppressorCount} times before being suppressed`);
+                        }
+                    }
+                })
+                .catch(error => {
+                    if (errorSuppressorCount < maxFetchErrorDisplays) {
+                        errorSuppressorCount++;
+                        this.localStorageMgr.setItem(errorSuppressorKey, JSON.stringify(errorSuppressorCount));
+                        window.alert(`The following error occurred during fetching. Please include this message when reporting this issue.\n${error}\n` +
+                            `This message will be reported ${maxFetchErrorDisplays - errorSuppressorCount} times before being suppressed`);
+                    }
                 });
         }
     }
