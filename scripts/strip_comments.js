d3bbdb12acbf5344c45c027358a506147097c035
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-04-23 13:27:54 -0400

    Testing strip comments

--- scripts/strip_comments.js
+++ scripts/strip_comments.js
@@ -5,15 +5,20 @@ const args = process.argv;
 const scriptPath = args[2];
 const outputDir = args[3];
 
+console.log('fdsa');
+
 function stripComments(scriptPath, outputDir) {
     const options = {
         ignore: /(?!\/\* globals)/g
     };
     gulp.task('default', function () {
+        console.log('in task');
         return gulp.src(scriptPath)
             .pipe(strip(options))
             .pipe(gulp.dest(outputDir));
     });
 }
 
+console.log('fdsa2');
 stripComments(scriptPath, outputDir);
+console.log('fdsa3');
