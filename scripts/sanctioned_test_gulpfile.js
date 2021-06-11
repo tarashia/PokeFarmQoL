f1c886339bfb6d9623c37690328d407ce6e30096
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2021-06-11 18:15:58 -0400

    Refactor existing gulp files

--- scripts/sanctioned_test_gulpfile.js
+++ scripts/sanctioned_test_gulpfile.js
@@ -5,44 +5,14 @@ const replace = require('gulp-replace');
 const eslint = require('gulp-eslint');
 const fs = require('fs');
 const path = require('path');
+const sources = require('./sources');
 
 const output = 'Poke-Farm-QoL.sanctioned.test.user.js';
 const outputDir = path.join(__dirname, '..');
 const outputFullPath = path.join(outputDir, output);
 
-const commonSources = [
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/polyfill.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/resources.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/helpers.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/globals.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/localStorageManager.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/qolHub.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/basePage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/daycarePage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/dexPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/farmPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/fishingPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/labPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/multiuserPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/privateFieldsPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/publicFieldsPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/shelterPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/wishforgePage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/pagesManager.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/pfqol.js',
-];
-const sanctionedSources = [
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/resources.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/globals.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/localStorageManager.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/qolHub.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/shelterPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/privateFieldsPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/labPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/farmPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/dexPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/sanctioned/pfqol.js',
-];
+const commonSources = sources.commonSources;
+const sanctionedSources = sources.sanctionedSources;
 
 function concatenate() {
     return src(
--- scripts/user_test_gulpfile.js
+++ scripts/user_test_gulpfile.js
@@ -5,47 +5,14 @@ const replace = require('gulp-replace');
 const eslint = require('gulp-eslint');
 const fs = require('fs');
 const path = require('path');
+const sources = require('./sources');
 
 const output = 'Poke-Farm-QoL.test.user.js';
 const outputDir = path.join(__dirname, '..');
 const outputFullPath = path.join(outputDir, output);
 
-const commonSources = [
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/polyfill.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/resources.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/helpers.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/globals.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/localStorageManager.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/qolHub.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/basePage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/daycarePage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/dexPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/farmPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/fishingPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/labPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/multiuserPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/privateFieldsPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/publicFieldsPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/shelterPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/wishforgePage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/pagesManager.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/common/pfqol.js',
-];
-const userSources = [
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/resources.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/globals.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/evolutionTreeParser.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/dexPageParser.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/localStorageManager.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/dexUtilities.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/qolHub.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/shelterPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/privateFieldsPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/labPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/farmPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/dexPage.js',
-    '/home/jonpaul/code/javascript/PokeFarmQoL/requires/user/pfqol.js',
-];
+const commonSources = sources.commonSources;
+const userSources = sources.userSources;
 
 function concatenate() {
     return src(
