a7c98545f4b9736d21e4190a117ad72a57fac551
kareesnc <108560481+tarashia@users.noreply.github.com>
2022-07-28 23:03:35 -0400

    Minor: Moved a bit of hub html that got missed before

--- src/scripts/qolHub.js
+++ src/scripts/qolHub.js
@@ -219,14 +219,6 @@ class QoLHub {
             this.jQuery('.textareahub textarea', document).val(customCss);
         }
 
-        const dexUpdateRowContents = `<td colspan="2" class="qolAllSettings">
-          <span>Notice that you can't find the newly added Eggs or Pokemon in shelter?
-          You may have to update your pokedex. Please visit the Dex page, and the Userscript will update itself with
-          the newest pokemon. Then, in order to use the update, refresh the page where you are using the script's search features.</span><br>
-          <span>Date last updated:<span class="qolDate">""</span></span>
-          </td>`;
-        this.jQuery('#qolDexUpdateRow').append(dexUpdateRowContents);
-
         const dexUpdateDate = (this.GLOBALS.DEX_UPDATE_DATE === null) ?
             'Not updated since installation' :
             this.GLOBALS.DEX_UPDATE_DATE;
