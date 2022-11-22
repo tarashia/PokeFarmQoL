da27b49f07b868158b6fdb4899a1973cbc33db1e
kareesnc <108560481+tarashia@users.noreply.github.com>
2022-11-21 20:55:47 -0500

    Update scriptEntry.js
    Attempt to detect user login status - do not init when not logged in, as doing so may cause a "fatal error" to print to the screen

--- src/scripts/scriptEntry.js
+++ src/scripts/scriptEntry.js
@@ -4,14 +4,29 @@ $(function () {
   if (typeof(module) !== 'undefined') {
     module.exports.pfqol = PFQoL;
   } else {
+    // detect user login status - use to hide init error when logged out
+    let loggedIn = true;
     try {
-      new PFQoL();
+      if($('#core')[0].getAttribute('data-user')=='') {
+        loggedIn = false;
+      }
     } catch(err) {
-      let message = 'Fatal error initializing QoL'
-      console.error(message);
+      console.error('Could not determine user login status');
       console.error(err);
-      let errorMsg = Helpers.errorToString(message, 'error', err);
-      $('body').append('<div class="panel" style="padding:0.5rem;word-wrap:break-word;user-select:all;">'+errorMsg+'</div>');
+    }
+    if(loggedIn) {
+      try {
+        new PFQoL();
+      } catch(err) {
+        let message = 'Fatal error initializing QoL'
+        console.error(message);
+        console.error(err);
+        let errorMsg = Helpers.errorToString(message, 'error', err);
+        $('body').append('<div class="panel" style="padding:0.5rem;word-wrap:break-word;user-select:all;">'+errorMsg+'</div>');
+      }
+    }
+    else {
+      console.log('Not logged in - did not init QoL script');
     }
   }
 });
