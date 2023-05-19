64be828e60b541d3e09687783d312477c305d3b6
kareesnc <108560481+tarashia@users.noreply.github.com>
2023-05-18 23:21:29 -0400

    add shelter search tab w/ settings

--- src/scripts/features/shelter.js
+++ src/scripts/features/shelter.js
@@ -10,11 +10,12 @@ class Shelter {
             Helpers.addGlobalStyle(Resources.SHELTER_CSS);
             Helpers.addGlobalStyle(Resources.SEARCH_CSS);
             // if specific features are enabled, run them
-            if(settings[Shelter.SUB_SETTINGS].sort) {
-                this.setupSort(settings);
-            }
             if(settings[Shelter.SUB_SETTINGS].search) {
                 this.setupSearch(settings);
+            }
+            // putting this second will actually make its tab first
+            if(settings[Shelter.SUB_SETTINGS].sort) {
+                this.setupSort(settings);
             }
             settings.addSettingsListeners();
         }
@@ -23,34 +24,12 @@ class Shelter {
         }
     }
 
-    setupSearch(settings) {
-        // listen for next match hotkey
-        $(window).on('keyup', function (e) {
-            if (0 == $(e.target).closest('input, textarea').length) {
-                if(e.keyCode == Shelter.NEXT_MATCH_KEY) {
-                    console.log('TODO: next key pressed');
-                }
-            }
-        });
-        Helpers.addObserver(document.querySelector('#shelterarea'), {
-            childList: true
-        }, function(mutations) {
-            console.log('mutation observed');
-            console.log(mutations);
-            //this.runSearch(); //TODO
-        });
-        console.log('TODO: search enabled');
-    }
-    runSearch() {
-        console.warn('TODO: field search');
-    }
-
     setupSort(settings) {
         // add sort tab
-        document.querySelector('.tabbed_interface.horizontal>ul').insertAdjacentHTML('afterbegin', '<li class=""><label>Sort</label></li>');
-        document.querySelector('.tabbed_interface.horizontal>ul').insertAdjacentHTML('afterend', Resources.SHELTER_SORT_HTML);
+        document.querySelector('#shelterupgrades .tabbed_interface>ul').insertAdjacentHTML('afterbegin', '<li><label>Sort</label></li>');
+        document.querySelector('#shelterupgrades .tabbed_interface>ul').insertAdjacentHTML('afterend', '<div>'+Resources.SHELTER_SORT_HTML+'</div>');
 
-        // add settings change listener
+        // listen for sort settings changes
         settings.registerChangeListener(function(changeDetails) {
             if(changeDetails.settingName == 'shelterGrid' || changeDetails.settingName == 'shelterSpriteSize') {
                 Shelter.handleSortSettings();
@@ -78,5 +57,43 @@ class Shelter {
         else if(shelterSettings.shelterSpriteSize == 'small') {
             $('#shelterarea').addClass('qolshelterareasmall');
         }
+    }
+
+    setupSearch(settings) {
+        // add search tab & results box
+        $('#shelterupgrades .tabbed_interface>div').removeClass('tab-active');
+        $('#shelterupgrades .tabbed_interface>ul>li').removeClass('tab-active');
+        document.querySelector('#shelterupgrades .tabbed_interface>ul').insertAdjacentHTML('afterbegin', '<li class="tab-active"><label>Search</label></li>');
+        document.querySelector('#shelterupgrades .tabbed_interface>ul').insertAdjacentHTML('afterend', '<div class="tab-active">'+Resources.SHELTER_SEARCH_HTML+'</div>');
+        document.querySelector('#sheltercommands').insertAdjacentHTML('beforebegin', '<div id="sheltersuccess"></div>');
+
+        // listen for next match hotkey
+        $(window).on('keyup', function (e) {
+            if (0 == $(e.target).closest('input, textarea').length) {
+                if(e.keyCode == Shelter.NEXT_MATCH_KEY) {
+                    console.log('TODO: next key pressed');
+                }
+            }
+        });
+
+        // watch for shelter page refreshes (and initial load)
+        Helpers.addObserver(document.querySelector('#shelterarea'), {
+            childList: true
+        }, function(mutations) {
+            console.log('mutation observed');
+            console.log(mutations);
+            Shelter.runSearch();
+        });
+
+        // listen for seach settings changes
+        settings.registerChangeListener(function(changeDetails) {
+            if(changeDetails.settingGroup == Shelter.SETTING_KEY) {
+                Shelter.runSearch();
+            }
+        });
+        console.log('TODO: search enabled');
+    }
+    static runSearch() {
+        console.warn('TODO: field search');
     }
 }
